87daa5b39e411e475462d9da8f220e1b8e8ebd41
Kiran <kiran@potdar.com>
2022-11-25 11:51:26 +0530

    debugging firebase connection issue

--- utils/services.ts
+++ utils/services.ts
@@ -1,16 +1,30 @@
 import { db } from "./firebase";
-import { collection, doc, addDoc, serverTimestamp } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  addDoc,
+  serverTimestamp,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
 
 export async function createDocumentFile(input: string, email: string) {
   try {
-    const docRef = doc(db, "userDocs", email);
+    const docRef = doc(db, "docs", "WXUsSCRqDuOUqanC2pv5");
 
-    const newDoc = await addDoc(collection(db, "docs"), {
-      docId: docRef.id,
-      fileName: input,
-      createdAt: serverTimestamp(),
-    });
-    console.log(newDoc);
+    // const newDoc = {
+    //   docId: docRef.id,
+    //   fileName: input,
+    //   createdAt: new Date(Date.now()).toISOString(),
+    // };
+    // console.log(newDoc);
+    const document = await getDoc(docRef);
+
+    if (document.exists()) {
+      console.log(document.data());
+    } else {
+      throw new Error("failed to fetch doc");
+    }
   } catch (err) {
     console.log(err);
   }
